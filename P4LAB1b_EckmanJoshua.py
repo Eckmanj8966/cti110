f56f936ac271f767aa43c69f497eecd034cec2a2
Eckmanj8966 <124816827+Eckmanj8966@users.noreply.github.com>
2023-03-30 13:46:07 -0400

    Writes my Initials with a turtle

---
+++ P4LAB1b_EckmanJoshua.py
@@ -0,0 +1,37 @@
+# create turtle
+import turtle
+dave = turtle.Turtle()
+
+# set attributes
+dave.color("purple")
+dave.pensize(2)
+
+# draw first initial
+dave.forward(50)
+dave.backward(25)
+dave.right(90)
+dave.forward(60)
+dave.right(120)
+dave.forward(30)
+
+# move turtle
+dave.penup()
+dave.right(150)
+dave.forward(75)
+dave.right(90)
+dave.forward(19)
+dave.left(90)
+dave.pendown()
+
+# draw second initial
+dave.forward(20)
+dave.backward(30)
+dave.left(90)
+dave.forward(30)
+dave.right(90)
+dave.forward(30)
+dave.backward(30)
+dave.left(90)
+dave.forward(30)
+dave.right(90)
+dave.forward(30)
